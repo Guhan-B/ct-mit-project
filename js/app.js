4cf17c15b7f82fdf7910eede6f6a570fbcfb1509
Guhan <bkguhan2001@gmail.com>
2020-09-16 18:48:40 +0530

    created responsive navbar

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+document.querySelector('.toggle').addEventListener("click",function(){
+    document.querySelector('.navbar').classList.toggle('showing')
+    document.querySelector('.navbar').classList.toggle('hidden')
+    console.log('clicked')
+    console.log(document.querySelector('.navbar').getElementsByClassName.transform)
+});
