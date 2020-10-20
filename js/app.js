91d8e909381f3a07ac5653a97189e15e18e5faa1
Guhan <bkguhan2001@gmail.com>
2020-10-20 20:31:41 +0530

    Updated About page and research page styles

--- js/app.js
+++ js/app.js
@@ -1,22 +1,26 @@
+let backToTop = document.getElementById("back-to-top")
 
-document.querySelector('.toggle').addEventListener("click",function(){
-    document.querySelector('.navbar').classList.toggle('showing')
-    document.querySelector('.navbar').classList.toggle('hidden')
-    console.log('clicked')
-    console.log(document.querySelector('.navbar').getElementsByClassName.transform)
-});
+window.onscroll = () => {
 
-// slide show js
+  if (document.documentElement.scrollTop > 20) {
+    backToTop.style.display = "block"
+  }
+  else {
+    backToTop.style.display = "none"
+  }
+}
+
+backToTop.addEventListener("click", () => {
+  document.documentElement.scrollTo(0, 0)
+})
 
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -25,19 +29,21 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
 
-  slides[slideIndex-1].style.display = "block";
+var date = new Date();
+document.getElementById("year").innerHTML = "Copyright &copy; " + date.getFullYear() + " Computer Technology | Designed by MIT Web Team";
 
-} 
-
-setInterval(function(){
+setInterval(()=>{
   plusSlides(1);
-},7000)
-
-var date = new Date()
-document.getElementById("year").innerHTML = "Copyright &copy; "+date.getFullYear()+ " Computer Technology | Designed by MIT Web Team"
+},3000)
---
+++ js/navbar.js
@@ -0,0 +1,6 @@
+document.querySelector('.toggle').addEventListener("click", function () {
+    document.querySelector('.navbar').classList.toggle('showing')
+    document.querySelector('.navbar').classList.toggle('hidden')
+    console.log('clicked')
+    console.log(document.querySelector('.navbar').getElementsByClassName.transform)
+  });
